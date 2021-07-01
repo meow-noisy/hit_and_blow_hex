d9738859905b6f8fc2e51896dfd1008795d5a24a
meow <meow.noisy.code@gmail.com>
2021-07-01 21:34:22 +0900

    refactoring. into component

---
+++ src/components/HitAndBlow.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >0</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >1</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >2</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >3</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >4</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >5</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >6</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >7</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >8</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >9</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >A</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >B</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >C</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >D</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >E</v-btn
+          >
+          <v-btn color="primary" elevation="2" height="100" width="100"
+            >F</v-btn
+          >
+          <v-btn color="accent" elevation="2" height="100" width="100"
+            >submit</v-btn
+          >
+          <v-btn color="accent" elevation="2" height="100" width="100"
+            >clear</v-btn
+          >
+          <v-btn color="accent" elevation="2" height="100" width="100"
+            >give up</v-btn
+          >
+          <v-btn color="accent" elevation="2" height="100" width="100"
+            >reset</v-btn
+          >
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HitAndBlow",
+};
+</script>
