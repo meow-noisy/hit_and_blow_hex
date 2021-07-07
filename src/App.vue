0dc94a1842dd9d78c1a8afc3f473cca8ac5ef9c5
meow <meow.noisy.code@gmail.com>
2021-07-07 19:23:27 +0900

    add event function which adds number into estimate array

--- src/App.vue
+++ src/App.vue
@@ -10,52 +10,127 @@
               elevation="2"
               height="100"
               width="100"
-              v-on:click="changeText(0)"
+              v-on:click="addNumToEstimateArray('0')"
               >0</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('1')"
               >1</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('2')"
               >2</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('3')"
               >3</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('4')"
               >4</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('5')"
               >5</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('6')"
               >6</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('7')"
               >7</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('8')"
               >8</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('9')"
               >9</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('A')"
               >A</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('B')"
               >B</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('C')"
               >C</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('D')"
               >D</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('E')"
               >E</v-btn
             >
-            <v-btn color="primary" elevation="2" height="100" width="100"
+            <v-btn
+              color="primary"
+              elevation="2"
+              height="100"
+              width="100"
+              v-on:click="addNumToEstimateArray('F')"
               >F</v-btn
             >
             <v-btn color="accent" elevation="2" height="100" width="100"
@@ -87,12 +162,17 @@ export default {
 
   data: () => ({
     estimate_array: [],
-    message: "hello",
   }),
 
+  computed: {
+    message: function () {
+      return this.estimate_array;
+    },
+  },
+
   methods: {
-    changeText: function (num) {
-      this.message = num;
+    addNumToEstimateArray: function (num) {
+      this.estimate_array.push(num);
     },
   },
 };
