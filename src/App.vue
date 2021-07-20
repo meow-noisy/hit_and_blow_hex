877ecbc8558c22745376bf07d72aba583e236e64
meow <meow.noisy.code@gmail.com>
2021-07-20 20:27:00 +0900

    attach dialog to GIVE UP button

--- src/App.vue
+++ src/App.vue
@@ -37,9 +37,45 @@
               v-on:click="clearEstimateArrayElements()"
               >clear</v-btn
             >
-            <v-btn color="accent" elevation="2" height="100" width="100"
-              >give up</v-btn
-            >
+
+            <v-dialog v-model="dialog" width="500">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="accent"
+                  elevation="2"
+                  height="100"
+                  width="100"
+                  v-bind="attrs"
+                  v-on="on"
+                  >give up</v-btn
+                >
+              </template>
+
+              <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                  Really give up?
+                </v-card-title>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn rounded elevation="2" text @click="dialog = false">
+                    No.
+                  </v-btn>
+
+                  <v-btn
+                    rounded
+                    elevation="2"
+                    color="accent"
+                    @click="dialog = false"
+                  >
+                    Yes, I give up.
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+
             <v-btn
               color="accent"
               elevation="2"
@@ -48,39 +84,6 @@
               v-on:click="setAnswerNumberGenerator()"
               >reset</v-btn
             >
-            <v-dialog v-model="dialog" width="500">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
-                  Click Me
-                </v-btn>
-              </template>
-
-              <v-card>
-                <v-card-title class="text-h5 grey lighten-2">
-                  Privacy Policy
-                </v-card-title>
-
-                <v-card-text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </v-card-text>
-
-                <v-divider></v-divider>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="primary" text @click="dialog = false">
-                    I accept
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
           </v-card>
 
           <v-card class="elevation-12">
