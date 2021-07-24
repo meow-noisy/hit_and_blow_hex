be073e10ee3403940f927eba708d44ce43a03fd5
meow <meow.noisy.code@gmail.com>
2021-07-24 13:23:31 +0900

    tuning some buttons position

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-container fluid fill-height>
       <v-layout align-center justify-center>
-        <v-flex xs12 sm7 md7>
+        <v-flex xs12 sm6 md6>
           <v-card class="elevation-0">
             {{ message }}<br />
             {{ current_predict }}
@@ -21,78 +21,81 @@
             >
           </v-card>
 
-          <v-card class="elevation-12">
-            <v-btn
-              color="accent"
-              elevation="2"
-              height="100"
-              width="100"
-              v-bind:disabled="submit_button_disable"
-              v-on:click="count_num_of_hit_and_blow()"
-              >submit</v-btn
-            >
-            <v-btn
-              color="accent"
-              elevation="2"
-              height="100"
-              width="100"
-              v-on:click="clearEstimateArrayElements()"
-              v-bind:disabled="clear_disabled"
-              >clear</v-btn
-            >
+          <v-col class="text-right">
+            <v-card class="elevation-12">
+              <v-btn
+                color="accent"
+                elevation="2"
+                height="100"
+                width="100"
+                v-bind:disabled="submit_button_disable"
+                v-on:click="count_num_of_hit_and_blow()"
+                >submit</v-btn
+              >
+              <v-btn
+                color="accent"
+                elevation="2"
+                height="100"
+                width="100"
+                v-on:click="clearEstimateArrayElements()"
+                v-bind:disabled="clear_disabled"
+                >clear</v-btn
+              >
 
-            <v-dialog v-model="dialog" width="500">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  color="accent"
-                  elevation="2"
-                  height="100"
-                  width="100"
-                  v-bind="attrs"
-                  v-on="on"
-                  v-bind:disabled="giveup_disabled"
-                  >give up</v-btn
-                >
-              </template>
-
-              <v-card>
-                <v-card-title class="text-h5 grey lighten-2">
-                  Really give up?
-                </v-card-title>
-
-                <v-divider></v-divider>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn rounded elevation="2" text @click="dialog = false">
-                    No.
-                  </v-btn>
-
+              <v-dialog v-model="dialog" width="500">
+                <template v-slot:activator="{ on, attrs }">
                   <v-btn
-                    rounded
-                    elevation="2"
                     color="accent"
-                    @click="dialog = false"
-                    v-on:click="goGaveOverState()"
+                    elevation="2"
+                    height="100"
+                    width="100"
+                    v-bind="attrs"
+                    v-on="on"
+                    v-bind:disabled="giveup_disabled"
+                    >give up</v-btn
                   >
-                    Yes, I give up.
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+                </template>
 
-            <v-btn
-              color="red"
-              class="white--text"
-              elevation="2"
-              height="100"
-              width="100"
-              v-show="show"
-              v-on:click="retryGame()"
-              >retry
-            </v-btn>
-          </v-card>
+                <v-card>
+                  <v-card-title class="text-h5 grey lighten-2">
+                    Really give up?
+                  </v-card-title>
+
+                  <v-divider></v-divider>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn rounded elevation="2" text @click="dialog = false">
+                      No.
+                    </v-btn>
+
+                    <v-btn
+                      rounded
+                      elevation="2"
+                      color="accent"
+                      @click="dialog = false"
+                      v-on:click="goGaveOverState()"
+                    >
+                      Yes, I give up.
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+
+              <v-btn
+                color="red"
+                class="white--text"
+                elevation="2"
+                height="100"
+                width="100"
+                v-show="show"
+                v-on:click="retryGame()"
+                >retry
+              </v-btn>
+            </v-card>
+          </v-col>
         </v-flex>
+
         <v-flex xs12 sm4 md4>
           <v-card class="elevation-12">
             <v-simple-table>
